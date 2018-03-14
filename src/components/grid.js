66cbdd2ba3cdc5599bee4d69f1d2a3a7787adf87
github username <acc.tung@gmail.com>
2018-03-14 16:22:52 -0400

    grid

---
+++ src/components/grid.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+class Grid extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1> The Game of Life </h1>
+      </div>
+    )
+  }
+}
+
+export default Grid
