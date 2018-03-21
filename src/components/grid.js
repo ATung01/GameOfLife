63077b82394f2bf000cc4200371b8dc35e942268
github username <acc.tung@gmail.com>
2018-03-21 16:10:40 -0400

    fixed width

--- src/components/grid.js
+++ src/components/grid.js
@@ -4,10 +4,8 @@ import Box from './box'
 
 class Grid extends React.Component {
 
-
-
-
   render() {
+    const width = this.props.columns * 14
     let rowArr = []
     for (let x = 0; x < this.props.columns; x++) {
       for (let y = 0; y < this.props.rows; y++) {
@@ -21,7 +19,7 @@ class Grid extends React.Component {
 
 
     return (
-      <div className = "grid teal">
+      <div className = "grid teal" style = {{width: width}}>
 
         {rowArr}
         {console.log(this.props.columns)}
