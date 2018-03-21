a488bb1dca700d375fb68eeee5407e9d85b6045d
github username <acc.tung@gmail.com>
2018-03-21 18:33:13 -0400

    key not working

--- src/components/box.js
+++ src/components/box.js
@@ -3,9 +3,13 @@ import React from 'react'
 
 class Box extends React.Component {
 
+  handleClick = (event) => {
+    console.log(this.props)
+  }
+
   render() {
     return (
-      <div className = "box" >
+      <div className = "box" key = {this.props.key} onClick  = {this.handleClick}>
       </div>
     )
   }
--- src/components/grid.js
+++ src/components/grid.js
@@ -9,9 +9,10 @@ class Grid extends React.Component {
     let rowArr = []
     for (let x = 0; x < this.props.columns; x++) {
       for (let y = 0; y < this.props.rows; y++) {
+        let boxID = x + "_" + y
         rowArr.push(
           <Box
-            key ={x + "_" + y}
+            key = {boxID}
           />
         )
       }
